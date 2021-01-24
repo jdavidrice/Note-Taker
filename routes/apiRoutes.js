8f173c195d238799d6d04f1a2220350828b608fd
Jeremy Rice <jdavidrice@gmail.com>
2021-01-23 20:00:23 -0500

    Finish apiRoutes.js to display notes page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,19 +6,19 @@
 
 //   * DELETE`/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.This means you'll need to find a way to give each note a unique `id` when it's saved.In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
-let notesData = require("../db/db.json");
-const fs = require("fs");
+let savedNotes = require("../db/db"); 
+const fs = require("fs"); 
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
-    return res.json(notesData);
+    return res.json(savedNotes);
   });
   app.post("/api/notes", function (req, res) {
     let newNote = req.body;
-    newNote.id = notesData.length.toString();
-    notesData.push(newNote);
+    newNote.id = savedNotes.length.toString();
+    savedNotes.push(newNote);
 
-    fs.writeFile("./public/assets/db/db.json", JSON.stringify(notesData),(err) => {
+    fs.writeFile("./db/db", JSON.stringify(savedNotes),(err) => {
       if(err) {
         console.log(err);
         res.sendStatus(404);
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// const bodyParser = require('body-parser');
-const express = require('express');
-// const fs = require('fs');
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
 const app = express();
-// const path = require('path');
-const PORT = process.env.PORT || 8500;
 const path = require("path");
+const PORT = process.env.PORT || 8500;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static)(path.join(__dirname, "public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: " + PORT);
+  
 });
