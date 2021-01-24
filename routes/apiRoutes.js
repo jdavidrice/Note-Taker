c8e1bd27942921df24ea13dd340584d1f3aa424a
Jeremy Rice <jdavidrice@gmail.com>
2021-01-23 20:21:24 -0500

    Changed back to db.json; notes save properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@
 
 //   * DELETE`/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.This means you'll need to find a way to give each note a unique `id` when it's saved.In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
-let savedNotes = require("../db/db"); 
+let savedNotes = require("../db/db.json"); 
 const fs = require("fs"); 
 
 module.exports = function (app) {
@@ -18,7 +18,7 @@ module.exports = function (app) {
     newNote.id = savedNotes.length.toString();
     savedNotes.push(newNote);
 
-    fs.writeFile("./db/db", JSON.stringify(savedNotes),(err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes),(err) => {
       if(err) {
         console.log(err);
         res.sendStatus(404);
